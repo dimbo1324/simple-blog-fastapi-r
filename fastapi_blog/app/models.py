c2b50828e8c3592e0b1d57c5e0b22d4a0b7930b9
dimbo1324 <dim4dmi7rij@yandex.ru>
2026-03-08 11:49:28 +0300

    collapsed code in files

--- fastapi_blog/app/models.py
+++ fastapi_blog/app/models.py
@@ -7,7 +7,6 @@ from database import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
@@ -16,7 +15,6 @@ class User(Base):
         nullable=True,
         default=None,
     )
-
     posts: Mapped[list[Post]] = relationship(back_populates="author")
 
     @property
@@ -28,7 +26,6 @@ class User(Base):
 
 class Post(Base):
     __tablename__ = "posts"
-
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     content: Mapped[str] = mapped_column(Text, nullable=False)
@@ -41,5 +38,4 @@ class Post(Base):
         DateTime(timezone=True),
         default=lambda: datetime.now(UTC),
     )
-
     author: Mapped[User] = relationship(back_populates="posts")
