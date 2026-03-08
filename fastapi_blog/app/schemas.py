1febf94502c437076cf8781faff1075dc9a76f50
dimbo1324 <dim4dmi7rij@yandex.ru>
2026-03-08 11:46:38 +0300

    added ORM (sqlite logic), upgrated end-point funcs

--- fastapi_blog/app/schemas.py
+++ fastapi_blog/app/schemas.py
@@ -1,4 +1,20 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=150)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    image_file: str | None
+    image_path: str
 
 
 class PostBase(BaseModel):
@@ -8,11 +24,11 @@ class PostBase(BaseModel):
 
 
 class PostCreate(PostBase):
-    pass
+    user_id: int  # temporary
 
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
-
     id: int
     date_posted: str
+    author: UserResponse
