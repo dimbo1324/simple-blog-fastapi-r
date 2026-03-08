89ecfb6232680eba9f3c1492d4f714f30f56e27a
dimbo1324 <dim4dmi7rij@yandex.ru>
2026-03-08 09:49:57 +0300

    added post end-point

--- fastapi_blog/app/main.py
+++ fastapi_blog/app/main.py
@@ -1,10 +1,15 @@
-from data import posts
+import datetime
+
+from schemas import PostCreate, PostResponse
+from datetime import datetime
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.exceptions import RequestValidationError
 from fastapi import FastAPI, Request, HTTPException, status
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from data import posts
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -30,12 +35,28 @@ def post_page(request: Request, post_id: int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
     return posts
 
 
-@app.get("/api/posts/{post_id}")
+@app.post(
+    "/api/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": datetime.now().strftime("%B %d, %Y"),
+    }
+    posts.append(new_post)
+    return new_post
+
+
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
