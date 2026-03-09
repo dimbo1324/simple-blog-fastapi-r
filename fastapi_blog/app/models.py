3f411d52d1100fa66ebe2795a1620644603c7be5
dimbo1324 <dim4dmi7rij@yandex.ru>
2026-03-09 10:18:08 +0300

    added user's end points

--- fastapi_blog/app/models.py
+++ fastapi_blog/app/models.py
@@ -15,7 +15,9 @@ class User(Base):
         nullable=True,
         default=None,
     )
-    posts: Mapped[list[Post]] = relationship(back_populates="author")
+    posts: Mapped[list[Post]] = relationship(
+        back_populates="author", cascade="all, delete-orphan"
+    )
 
     @property
     def image_path(self) -> str:
