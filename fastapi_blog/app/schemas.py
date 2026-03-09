98a65b82515994e3b2e4fc4419267b3060cd8951
dimbo1324 <dim4dmi7rij@yandex.ru>
2026-03-09 10:21:46 +0300

    removed author  from class PostBase(BaseModel):

--- fastapi_blog/app/schemas.py
+++ fastapi_blog/app/schemas.py
@@ -27,7 +27,6 @@ class UserUpdate(BaseModel):
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_length=50)
 
 
 class PostCreate(PostBase):
