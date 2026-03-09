e7213d7225600db5af38338b06862bd91aab35aa
dimbo1324 <dim4dmi7rij@yandex.ru>
2026-03-09 09:57:41 +0300

    added uppdate and delete end points

--- fastapi_blog/app/main.py
+++ fastapi_blog/app/main.py
@@ -24,9 +24,9 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/media", StaticFiles(directory="media"), name="media")
 templates = Jinja2Templates(directory="templates")
+
+
 # ------------------------------------------
-
-
 @app.get("/", include_in_schema=False, name="home")
 @app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request, db: Annotated[Session, Depends(get_db)]):
