41bfcf0d4741c8c6568e286747a36bcc7b2b5da4
dimbo1324 <dimaprihodko180@gmail.com>
2026-03-12 15:52:58 +0300

    added asyncs

--- fastapi_blog/app/database.py
+++ fastapi_blog/app/database.py
@@ -1,23 +1,25 @@
 import os
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db/blog.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./db/blog.db"
 
 os.makedirs("db", exist_ok=True)
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class Base(DeclarativeBase):
     pass
 
 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
